e5e83709bcc29f253df367cb0a552cd44f1466a6
Lupinfan117 <46658033+Lupinfan117@users.noreply.github.com>
2023-10-21 20:37:22 -0500

    Update 0001_initial.py

--- Management/migrations/0001_initial.py
+++ Management/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('Management', '0001_initial'),
+        ('auth', '0012_alter_user_first_name_max_length'),
     ]
 
     operations = [
